020bbee49d6c887c197abe38603a01c77d857ed6
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-10-30 20:47:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,30 @@
 def create_an_empty_array
+  array = []
   
 end
 
 def create_an_array
+  array = ["hey", "hello", "hola", "zdravo"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  array.push(element)
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
+  array.pop(element)
   
 end
 
 def remove_element_from_start_of_array(array)
+  array.shift(element)
   
 end
 
